ec554121b9c35bfa2581c306986ee05ba8e46254
Adit <aditshah2002@gmail.com>
2020-10-08 11:29:32 +0530

    Background changed and text-glitch effect added

--- js/app.js
+++ js/app.js
@@ -2,61 +2,60 @@
 
 particlesJS('particles-js',
   {
-  "particles": {
-    "number": {
-      "value": 50,
-      "density": {
-        "enable": true,
-        "value_area": 700 // Denser the smaller the number.
-      }
-    },
-    "color": { // The color for every node, not the connecting lines.
-      "value": "#ffffff" // Or use an array of colors like ["#9b0000", "#001378", "#0b521f"]
-    },
-    "shape": {
-        "type": "circle", // Can show circle, edge (a square), triangle, polygon, star, img, or an array of multiple.
-        "stroke": { // The border
-          "width": 1,
-          "color": "#ffffff"
-        },
-        "polygon": { // if the shape is a polygon
-          "nb_sides": 5
-        },
-        "image": { // If the shape is an image
-          "src": "",
-          "width": 100,
-          "height": 100
+    "particles": {
+      "number": {
+        "value": 50,
+        "density": {
+          "enable": true,
+          "value_area": 700 // Denser the smaller the number.
         }
-    },
-    "opacity": {
-      "value": 0.7,
-      "random": true
-    },
-    "size": {
-      "value": 3,
-      "random": true
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150, // The radius before a line is added, the lower the number the more lines.
-      "color": "#ffffff",
-      "opacity": 0.5,
-      "width": 2  
-    },
-    "move": {
-      "enable": true,
-      "speed": 9,
-      "direction": "top", // Move them off the canvas, either "none", "top", "right", "bottom", "left", "top-right", "bottom-right" et cetera...
-      "random": true,
-      "straight": true, // Whether they'll shift left and right while moving.
-      "out_mode": "bounce ", // What it'll do when it reaches the end of the canvas, either "out" or "bounce".
-      "bounce": false,
-      "attract": { // Make them start to clump together while moving.
+      },
+      "color": { // The color for every node, not the connecting lines.
+        "value": "#01579b" // Or use an array of colors like ["#9b0000", "#001378", "#0b521f"]
+      },
+      "shape": {
+          "type": "star", // Can show circle, edge (a square), triangle, polygon, star, img, or an array of multiple.
+          "stroke": { // The border
+            "width": 1,
+            "color": "#145ea8"
+          },
+          "polygon": { // if the shape is a polygon
+            "nb_sides": 5
+          },
+          "image": { // If the shape is an image
+            "src": "",
+            "width": 100,
+            "height": 100
+          }
+      },
+      "opacity": {
+        "value": 0.7,
+        "random": true
+      },
+      "size": {
+        "value": 10,
+        "random": true
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 200, // The radius before a line is added, the lower the number the more lines.
+        "color": "#ffffff",
+        "opacity": 0.5,
+        "width": 2
+      },
+      "move": {
         "enable": true,
-        "rotateX": 1200,
-        "rotateY": 1200
+        "speed": 2,
+        "direction": "top", // Move them off the canvas, either "none", "top", "right", "bottom", "left", "top-right", "bottom-right" et cetera...
+        "random": true,
+        "straight": false, // Whether they'll shift left and right while moving.
+        "out_mode": "out", // What it'll do when it reaches the end of the canvas, either "out" or "bounce".
+        "bounce": false,
+        "attract": { // Make them start to clump together while moving.
+          "enable": true,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
       }
     }
-  }
- }
-);
+  })
