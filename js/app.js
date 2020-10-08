9f1d9898fe1863876dc040c5100bfc7ad84d7f77
Adit <aditshah2002@gmail.com>
2020-10-08 19:41:25 +0530

    Background changed to big triangles

--- js/app.js
+++ js/app.js
@@ -4,23 +4,23 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 300,
+        "value": 63,
         "density": {
           "enable": true,
           "value_area": 700 // Denser the smaller the number.
         }
       },
       "color": { // The color for every node, not the connecting lines.
-        "value": "#ffffff" // Or use an array of colors like ["#9b0000", "#001378", "#0b521f"]
+        "value": "#0000ff" // Or use an array of colors like ["#9b0000", "#001378", "#0b521f"]
       },
       "shape": {
           "type": "star", // Can show circle, edge (a square), triangle, polygon, star, img, or an array of multiple.
           "stroke": { // The border
-            "width": 0.5,
-            "color": "#ffffff"
+            "width": 0,
+            "color": "#0000ff"
           },
           "polygon": { // if the shape is a polygon
-            "nb_sides": 5
+            "nb_sides": 6
           },
           "image": { // If the shape is an image
             "src": "",
@@ -33,19 +33,19 @@ particlesJS('particles-js',
         "random": true
       },
       "size": {
-        "value": 4,
+        "value": 45 ,
         "random": true
       },
       "line_linked": {
         "enable":false,
-        "distance": 250, // The radius before a line is added, the lower the number the more lines.
-        "color": "#ffffff",
+        "distance": 200, // The radius before a line is added, the lower the number the more lines.
+        "color": "#0000ff",
         "opacity": 0.5,
         "width": 2
       },
       "move": {
         "enable": true,
-        "speed": 5,
+        "speed": 2,
         "direction": "top", // Move them off the canvas, either "none", "top", "right", "bottom", "left", "top-right", "bottom-right" et cetera...
         "random": true,
         "straight": false, // Whether they'll shift left and right while moving.
